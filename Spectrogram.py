b64bafbdb8d6f17e047d9188fd51f743b4d45e9c
Ahmad <ahmedmageed19@gmail.com>
2020-04-21 23:05:35 +0200

    added feature extraction

--- Spectrogram.py
+++ Spectrogram.py
@@ -1,5 +1,7 @@
 from scipy import signal
 import json
+import librosa as l
+from numpy import ndarray
 
 class spectrogram():
     """
@@ -12,8 +14,8 @@ class spectrogram():
         """
         Class Initializer
 
-        parameters
-        ```````````
+        Parameters
+        -----------
         - sampleFreqs : holds the sampled frequencies
         - sampleTime : holds the sampled time rates
         - colorMesh : holds the value (intenisty) of the frequency component
@@ -23,37 +25,43 @@ class spectrogram():
         self.sampleFreqs = None
         self.sampleTime = None
         self.colorMesh = None
-        self.container = {}
+        self.features = None
+        self.container = None
 
-    def __call__(self, songData: "numpy.ndarray", songSR: int, window:str, songName:str,
-                 path:str = None, compressed: bool = False):
+    def __call__(self, songData: "numpy.ndarray", songSR: int, window:str, fileName:str,
+                 path:str = None, compressed: bool = False, featureize:bool = False):
         """
         Caller function for the class which maintains all it's implemented methods
 
-        parameters
-        ```````````
+        Parameters
+        -----------
         - songData: a numpy array of the read wav file
         - songSR : integer representing the sample rate
         - window : a str specifying the widow type used in creating the spectrogram
         - path : str if specified the file is saved in the given path
-        - songName : the name of the file to be saved
+        - fileName : if provided the spectrogram will be saved as a json file
         - compressed : if True only the color mesh will be saved
+        - featurize : if True spectrogram`s main features will be extracted and saved if specified saving
         """
         self._spectrogram(songData, songSR, window)
+        if featureize:
+            self.features= self._spectralFeatures(None, self.colorMesh, songSR,window)
         print("spectrogram created")
-        if path is None:
-            self._saveFormat('', songName, compressed=compressed)
-            print("saved in main directory .. ")
-        else:
-            self._saveFormat(path, songName, compressed=compressed)
-        print("spectrogram saved")
+
+        if fileName:
+            if path is None:
+                self._saveFormat('', fileName, compressed=compressed, featurize=featureize)
+                print("saved in main directory .. ")
+            else:
+                self._saveFormat(path, fileName, compressed=compressed, featurize=featureize)
+            print("spectrogram saved")
 
     def _spectrogram(self, songData: "numpy.ndarray", songSampleRate:int, windowType: str):
         """
         Creates a Spectrogram of the given data
 
-        parameters
-        ``````````
+        Parameters
+        -----------
         - songData : a numpy array of the read wav file
         - songSampleRate : integer representing the sample rate
         - windowType : a str specifying the widow type used in creating the spectrogram
@@ -67,34 +75,78 @@ class spectrogram():
             self.sampleFreqs, self.sampleTime, self.colorMesh = signal.spectrogram(songData,
                                                                                    fs=songSampleRate, window=windowType)
 
-    def _saveFormat(self, folder:str, filename:str, compressed: bool = False):
+    def _saveFormat(self, folder:str, filename:str, featurize : bool = False, compressed: bool = False):
         """
         Save the spectrum in a specified filename.json
 
-        :parameters
-        ```````````
+        Parameters
+        -----------
         - folder : a path to the file location
         - filename : the file name
         - compressed : if True only the color mesh will be saved
+        - featurize : if True adds the main features of the spectrogram
         """
+        # TODO : Refactor this part .. return function
         if compressed:
             self.container = {"color_mesh": self.colorMesh.tolist()}
         else:
             self.container = {'sample_frequencies': self.sampleFreqs.tolist(),
                               "sample_time": self.sampleTime.tolist(),
                               "color_mesh": self.colorMesh.tolist()}
+        if featurize:
+            self.container['features'] = self.features
 
         with open(folder+filename+".json", 'w') as outfile:
             json.dump(self.container, outfile)
 
+    def _spectralFeatures(self, song: "np.ndarray"= None, S: "np.ndarray" = None, sr: int = 22050, window:'str'='hann'):
+        """
+        Calculates the Spectral Centroid of a given data or the data instantiated in the class
+        Parameters
+        -----------
+        - song  : wav file array
+        - S    : spectrogram readings
+        - sr : sampling frequency default 22050
+        - window: a string specifying the window applied default hann (see options)
+
+        Options
+        -------
+        - boxcar
+        - triang
+        - blackman
+        - hamming
+        - hann
+        - bartlett
+        - flattop
+        - parzen
+        - bohman
+        - blackmanharris
+        - nuttall
+        - barthann
+        - kaiser (needs beta)
+        - gaussian (needs standard deviation)
+        - general_gaussian (needs power, width)
+        - slepian (needs width)
+        - dpss (needs normalized half-bandwidth)
+        - chebwin (needs attenuation)
+        - exponential (needs decay scale)
+        - tukey (needs taper fraction)
+        """
+        if not (song or sr) :
+            print('provide either a wav file data or a spectrogram readings or both')
+        else:
+            return ndarray(l.feature.spectral_centroid(y= song, sr=sr, S=S,window = window),
+                           l.feature.spectral_rolloff(y= song, sr=sr, S=S,window = window),
+                           l.feature.melspectrogram(y= song, sr=sr, S=S,window = window))
 
 if __name__ == '__main__':
     # Basic Usage
 
     from scipy.io import wavfile
-    sampleRate, songdata = wavfile.read("Adele_Million_Years_Ago_10.wav")
+    sampleRate, songdata = wavfile.read("tests/Adele_Million_Years_Ago_10.wav")
     spectrum = spectrogram()
-    spectrum(songdata, sampleRate, window='hann', songName="test", compressed=True, path='tests/')
+    # spectrum(songdata, sampleRate, window='hann', songName="test", compressed=True, path='tests/')
+    spectrogram()._spectralCentroid()
 
 
 
