3a9b868a11b386e882247d9208d6f10e91e85fbb
Abdullah Alrefaey <abdullah.m.alrefaey@gmail.com>
2020-04-23 04:18:18 +0200

    Hash v1.2

--- hash.py
+++ hash.py
@@ -7,27 +7,52 @@ from pydub import AudioSegment
 
 weight = 0.8
 
-audioFile1 = AudioSegment.from_mp3("Songs/Adele_Million_Years_Ago_10.mp3")[:60000]
-songData1 = np.array(audioFile1.get_array_of_samples())
-sampleRate1 = audioFile1.frame_rate
-song1DataType = songData1.dtype
+# Load Files
+def loadAudioFile(filePath: str, fSeconds: float = None) -> dict:
+    """
+    Loads any audio file
 
-audioFile2 = AudioSegment.from_mp3("Songs/ImagineDragons_natural_10.mp3")[:60000]
-songData2 = np.array(audioFile2.get_array_of_samples())
-sampleRate2 = audioFile2.frame_rate
+    :param filePath: relative path of the file
+    :param fSeconds: number of seconds you want to load, if not it will load all the file
+    :return: Dictionary contains songName, pydub Object, array of the data, sample rate, dataType of the array
+    """
+    if fSeconds:
+        audioFile = AudioSegment.from_mp3(filePath)[:fSeconds]
+    else:
+        audioFile = AudioSegment.from_mp3(filePath)
+    songName = filePath.split('/')[-1]
+    songData = np.array(audioFile.get_array_of_samples())
+    sampleRate = audioFile.frame_rate
+    songDataType = songData.dtype
+    songDictionary = {
+        "name": songName,
+        "data": songData,
+        "sRate": sampleRate,
+        "dType": songDataType,
+    }
+    return songDictionary
 
-audioFile3 = AudioSegment.from_mp3("Songs/Adele_Million_Years_Ago_10_music.mp3")[:60000]
-songData3 = np.array(audioFile3.get_array_of_samples())
-sampleRate3 = audioFile2.frame_rate
+def mixSongs(song1: np.ndarray, song2: np.ndarray, dType: str = 'int16', w: float = 0.5) -> np.ndarray:
+    """
+    Mixes 2 songs with the given weight
+    :param song1: data array of the song1
+    :param song2: data array of the song2
+    :param dType: data type of the song
+    :param w: weight (percentage) of song1
+    :return array of the mixing songs
+    """
+    mixed = (w*song1 + (1.0-w)*song2).astype(dType)
+    return mixed
 
-# output = (weight*songData1 + (1.0-weight)*songData2).astype('int16')
-output = (weight*songData1 + (1.0-weight)*songData2).astype(song1DataType)
-print("output: \n", output)
+song1 = loadAudioFile("Songs/Adele_Million_Years_Ago_10.mp3", 60000)
+song2 = loadAudioFile("Songs/ImagineDragons_natural_10.mp3", 60000)
+song3 = loadAudioFile("Songs/Adele_Million_Years_Ago_10_music.mp3", 60000)
+mixedSong = mixSongs(song1['data'], song2['data'], w=0.8)
 
-sampleFreqs1, sampleTime1, colorMesh1 = signal.spectrogram(songData1, fs=sampleRate1, window='hann')
-sampleFreqs2, sampleTime2, colorMesh2 = signal.spectrogram(songData2, fs=sampleRate2, window='hann')
-sampleFreqs3, sampleTime3, colorMesh3 = signal.spectrogram(songData3, fs=sampleRate3, window='hann')
-sampleFreqs4, sampleTime4, colorMesh4 = signal.spectrogram(output, fs=sampleRate3, window='hann')
+sampleFreqs1, sampleTime1, colorMesh1 = signal.spectrogram(song1['data'], fs=song1['sRate'], window='hann')
+sampleFreqs2, sampleTime2, colorMesh2 = signal.spectrogram(song2['data'], fs=song2['sRate'], window='hann')
+sampleFreqs3, sampleTime3, colorMesh3 = signal.spectrogram(song3['data'], fs=song3['sRate'], window='hann')
+sampleFreqs4, sampleTime4, colorMesh4 = signal.spectrogram(mixedSong, fs=song1['sRate'], window='hann')
 
 
 def createPerceptualHash(arrayData: "np.ndarray") -> str:
