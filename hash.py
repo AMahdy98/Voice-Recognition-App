7db1c1a8bcd5e91e9ceb2fadbddc2558617de67b
Abdullah Alrefaey <abdullah.m.alrefaey@gmail.com>
2020-04-23 07:14:51 +0200

    Hash v1.3
    Edit load and hash function
    Added Mix and Similarity

--- hash.py
+++ hash.py
@@ -4,17 +4,16 @@ from imagehash import hex_to_hash
 import numpy as np
 from scipy import signal
 from pydub import AudioSegment
+import librosa as l
 
-weight = 0.8
 
-# Load Files
 def loadAudioFile(filePath: str, fSeconds: float = None) -> dict:
     """
     Loads any audio file
 
     :param filePath: relative path of the file
     :param fSeconds: number of seconds you want to load, if not it will load all the file
-    :return: Dictionary contains songName, pydub Object, array of the data, sample rate, dataType of the array
+    :return: Dictionary contains songName, array of the data, sample rate, dataType of the array
     """
     if fSeconds:
         audioFile = AudioSegment.from_mp3(filePath)[:fSeconds]
@@ -29,8 +28,32 @@ def loadAudioFile(filePath: str, fSeconds: float = None) -> dict:
         "data": songData,
         "sRate": sampleRate,
         "dType": songDataType,
+        "spectrogram_Hash": None,
+        "spectral_centroid_Hash": None,
+        "spectral_rolloff_Hash": None,
+        "melspectrogram_Hash": None,
+        "hammingDistance": None
     }
     return songDictionary
+
+
+def _spectralFeatures(song: "np.ndarray"= None, S: "np.ndarray" = None, sr: int = 22050, window: str = 'hann'):
+    """
+    Calculates the Spectral Centroid of a given data or the data instantiated in the class
+
+    Parameters
+    -----------
+    - song  : wav file array
+    - S    : spectrogram readings
+    - sr : sampling frequency default 22050
+    - window: a string specifying the window applied default hann (see options)
+    """
+    specFeatures = [None, None, None]
+    specFeatures[0] = l.feature.spectral_centroid(y= song, S=S, sr=sr, window = window)
+    specFeatures[1] = l.feature.spectral_rolloff(y= song, S=S, sr=sr, window = window)
+    specFeatures[2] = l.feature.melspectrogram(y= song, S=S, sr=sr, window = window)
+    return specFeatures
+
 
 def mixSongs(song1: np.ndarray, song2: np.ndarray, dType: str = 'int16', w: float = 0.5) -> np.ndarray:
     """
@@ -44,16 +67,6 @@ def mixSongs(song1: np.ndarray, song2: np.ndarray, dType: str = 'int16', w: floa
     mixed = (w*song1 + (1.0-w)*song2).astype(dType)
     return mixed
 
-song1 = loadAudioFile("Songs/Adele_Million_Years_Ago_10.mp3", 60000)
-song2 = loadAudioFile("Songs/ImagineDragons_natural_10.mp3", 60000)
-song3 = loadAudioFile("Songs/Adele_Million_Years_Ago_10_music.mp3", 60000)
-mixedSong = mixSongs(song1['data'], song2['data'], w=0.8)
-
-sampleFreqs1, sampleTime1, colorMesh1 = signal.spectrogram(song1['data'], fs=song1['sRate'], window='hann')
-sampleFreqs2, sampleTime2, colorMesh2 = signal.spectrogram(song2['data'], fs=song2['sRate'], window='hann')
-sampleFreqs3, sampleTime3, colorMesh3 = signal.spectrogram(song3['data'], fs=song3['sRate'], window='hann')
-sampleFreqs4, sampleTime4, colorMesh4 = signal.spectrogram(mixedSong, fs=song1['sRate'], window='hann')
-
 
 def createPerceptualHash(arrayData: "np.ndarray") -> str:
     """
@@ -66,27 +79,54 @@ def createPerceptualHash(arrayData: "np.ndarray") -> str:
     return dataHash.__str__()
 
 
-hash1 = createPerceptualHash(colorMesh1)
-hash2 = createPerceptualHash(colorMesh2)
-hash3 = createPerceptualHash(colorMesh3)
+def loadSong(filePath: str, fSeconds: float = None) -> dict:
+    """
+    Loads any audio file, create a spectrogram, extract some features and hash them.
+
+    :param filePath: relative path of the file
+    :param fSeconds: number of seconds you want to load, if not it will load all the file
+    :return: Dictionary contains songName, array of the data, sample rate, dataType of the array and the hashes
+    """
+    features = []
+    song = loadAudioFile(filePath, fSeconds)
+    sampleFreqs, sampleTime, colorMesh = signal.spectrogram(song['data'], fs=song['sRate'], window='hann')
+    features = _spectralFeatures(song=song['data'], S=colorMesh, sr=song['sRate'])
+    song['spectrogram_Hash'] = createPerceptualHash(colorMesh)
+    song['spectral_centroid_Hash'] = createPerceptualHash(features[0])
+    song['spectral_rolloff_Hash'] = createPerceptualHash(features[1])
+    song['melspectrogram_Hash'] = createPerceptualHash(features[2])
+    return song
+
+
+song1 = loadSong("Songs/Adele_Million_Years_Ago_10.mp3", 60000)
+song2 = loadSong("Songs/ImagineDragons_natural_10.mp3", 60000)
+song3 = loadSong("Songs/Adele_Million_Years_Ago_10_music.mp3", 60000)
+song4 = loadSong("Songs/Spacetoon_remi_11.mp3", 60000)
+
+songs = [song1, song2, song3, song4]
+
+mixedSong = mixSongs(song1['data'], song2['data'], w=0.8)
+sampleFreqs4, sampleTime4, colorMesh4 = signal.spectrogram(mixedSong, fs=song1['sRate'], window='hann')
 hashMix = createPerceptualHash(colorMesh4)
 
-hash1Hex = hex_to_hash(hash1)
-hash2Hex = hex_to_hash(hash2)
-hash3Hex = hex_to_hash(hash3)
-hashMixHex = hex_to_hash(hashMix)
+totalHashes = 0
+for song in songs:
+    song['hammingDistance'] = hex_to_hash(song['spectrogram_Hash']) - hex_to_hash(hashMix)
+    totalHashes += song['hammingDistance']
 
-result1 = hash1Hex - hashMixHex
-result2 = hash2Hex - hashMixHex
-result3 = hash3Hex - hashMixHex
-total = result1 + result2 + result3
-
-print("hash1: ", hash1)
-print("hash2: ", hash2)
-print("hash3: ", hash3)
+print("hash1: ", song1['spectrogram_Hash'])
+print("hash2: ", song2['spectrogram_Hash'])
+print("hash3: ", song3['spectrogram_Hash'])
+print("hash4: ", song4['spectrogram_Hash'])
 print("mixHash: ", hashMix)
 
-print("diff hash1 and mix", hash1Hex - hashMixHex)
-print("diff hash2 and mix", hash2Hex - hashMixHex)
-print("diff hash3 and mix", hash3Hex - hashMixHex)
+print("diff hash1 and mix", hex_to_hash(song1['spectrogram_Hash']) - hex_to_hash(hashMix))
+print("diff hash2 and mix", hex_to_hash(song2['spectrogram_Hash']) - hex_to_hash(hashMix))
+print("diff hash3 and mix", hex_to_hash(song3['spectrogram_Hash']) - hex_to_hash(hashMix))
+print("diff hash4 and mix", hex_to_hash(song4['spectrogram_Hash']) - hex_to_hash(hashMix))
+print("total Hashes", totalHashes)
 
+print(f"Similarity of song1 with the mix = {(1-song1['hammingDistance']/totalHashes)*100}%")
+print(f"Similarity of song2 with the mix = {(1-song2['hammingDistance']/totalHashes)*100}%")
+print(f"Similarity of song3 with the mix = {(1-song3['hammingDistance']/totalHashes)*100}%")
+print(f"Similarity of song4 with the mix = {(1-song4['hammingDistance']/totalHashes)*100}%")
